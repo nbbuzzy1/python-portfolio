82b1098a5af6ad4bc1bafd971cabe8e93afd5711
nbbuzzy1 <40919504+nbbuzzy1@users.noreply.github.com>
2020-06-22 07:31:39 -0400

    Add initial routes and templates.

--- server.py
+++ server.py
@@ -1,8 +1,22 @@
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
-print(__name__)
 
 
 @app.route('/')
 def hello_world():
-    return 'Hello, Nick!!!'
+    return render_template('index.html')
+
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+
+@app.route('/blog')
+def blog():
+    return 'These are my thoughts on blogs'
+
+
+@app.route('/blog/2020/dogs')
+def blog2():
+    return 'this is my dog'
